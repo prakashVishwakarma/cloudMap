a47015f2dab66c7673c12989f2090109b51dc355
prakashVishwakarma <prksvsvkrma@gmail.com>
2022-04-27 17:26:39 +0530

    coludmap

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Center from './component/Center.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Center />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Center.jsx
@@ -0,0 +1,216 @@
+import React from 'react';
+import "./Center.css";
+import cloud1 from "../assets/Cloud.png"
+import line1 from "../assets/line1.gif"
+import Reporting from "../assets/Reporting.png"
+import Real_time from "../assets/Real_time.gif"
+import First_customer from "../assets/First_customer.png"
+import Survey from "../assets/Survey.png"
+import Customer_Experience from "../assets/Customer_Experience.png"
+import shield from "../assets/shield.png"
+import circle from "../assets/circle.png"
+import line_2 from "../assets/line_2.gif"
+import House1 from "../assets/House1.png"
+import office_building from "../assets/office_building.png"
+import line_3 from "../assets/line_3.gif"
+import piston from "../assets/piston.png"
+import arrows_1 from "../assets/arrows_1.gif"
+import line_4 from "../assets/line_4.gif"
+import Conversational from "../assets/Conversational.png"
+import NLP from "../assets/NLP.png"
+import Contextual from "../assets/Contextual.png"
+import RPA from "../assets/RPA.png"
+import Sentiment from "../assets/Sentiment.png"
+import recognition from "../assets/recognition.png"
+import voice from "../assets/voice.png"
+import Voice_1 from "../assets/Voice_1.png"
+import Visual from "../assets/Visual.png"
+import Chatbot from "../assets/Chatbot.png"
+import line_5 from "../assets/line_5.gif"
+import line_6 from "../assets/line_6.gif"
+import omni_channel_bg from "../assets/omni_channel_bg.png"
+import girl from "../assets/girl.gif"
+import voice_2 from "../assets/voice_2.png"
+import voice_active_2 from "../assets/voice_active_2.png"
+import sms from "../assets/sms.png"
+import sms_active_3 from "../assets/sms_active_3.png"
+import social_media from "../assets/social_media.png"
+import social_media_active_1 from "../assets/social_media_active_1.png"
+import email from "../assets/email.png"
+import email_active_2 from "../assets/email_active_2.png"
+import white_mail from "../assets/white_mail.png"
+// import white_mail_2 from "../assets/white_mail_2.png"
+import white_mail_2 from "../assets/white_mail_active_1.png"
+import chat from "../assets/chat.png"
+import chat_active_2 from "../assets/chat_active_2.png"
+
+const Center = () => {
+    return (
+        <div className='center'>
+            <img src={line_4} alt="" className=' line_4' />
+            <div className="tooltip">
+                <h5 className='Associates_h5 '>Associates <br /> in Office</h5>
+                <span class="Associates_tooltiptext"><p>Real-time reporting, and dashboards give complete visibility and detailed drill-downs.</p></span>
+            </div>
+            <img src={cloud1} alt="" className='cloud' />
+            <div class="tooltip">
+                <img src={shield} alt="" className='shield' />
+                <span class="shield_tooltiptext"><p>Real-time reporting, and dashboards give complete visibility and detailed drill-downs.</p></span>
+            </div>
+            <img src={line1} alt="" className=' line1' />
+            <div class="tooltip">
+                <img src={Reporting} alt="" className=' Reporting ' />
+                <span class="tooltiptext"><p>Real-time reporting, and dashboards give complete visibility and detailed drill-downs.</p></span>
+            </div>
+            <div class="tooltip">
+                <img src={Real_time} alt="" className=' Real_time abs ' />
+                <span class="Real_time_tooltiptext "><p> not Real-time reporting, and dashboards give complete visibility and detailed drill-downs.</p></span>
+            </div>
+            <div class="tooltip">
+                <img src={Survey} alt="" className=' Survey ' />
+                <span class="Survey_tooltiptext"><p>1 not Real-time reporting, and dashboards give complete visibility and detailed drill-downs.</p></span>
+            </div>
+            <div class="tooltip">
+                <img src={Customer_Experience} alt="" className=' Customer_Experience ' />
+                <span class="Customer_Experience_tooltiptext"><p>Real-time reporting, and dashboards give complete visibility and detailed drill-downs.</p></span>
+            </div>
+            <img src={First_customer} alt="" className=' First_customer' />
+            <img src={line_2} alt="" className=' line_2' />
+            <img src={House1} alt="" className=' House1' />
+            <img src={office_building} alt="" className=' office_building' />
+            <img src={line_3} alt="" className=' line_3' />
+            <img src={piston} alt="" className=' piston' />
+            <img src={circle} alt="" className=' circle' />
+            <img src={arrows_1} alt="" className=' arrows_1' />
+            <img src={Conversational} alt="" className=' Conversational' />
+            <img src={NLP} alt="" className=' NLP' />
+            <img src={Contextual} alt="" className=' Contextual' />
+            <img src={RPA} alt="" className=' RPA' />
+            <img src={Sentiment} alt="" className=' Sentiment' />
+            <img src={recognition} alt="" className=' recognition' />
+
+
+
+
+
+
+
+            <div className="tooltip">
+                <p className='Biometric_h5 '>Biometric Authentication</p>
+                <span class="Biometric_tooltiptext"><p>Real-time reporting, and dashboards give complete visibility and detailed drill-downs.</p></span>
+            </div>
+                <p className='Conversianel_h5 '>Conversianel AI/ML</p>
+
+            <div className="tooltip">
+                <p className='Voice_h5 '>Voice Assistant</p>
+                <span class="Voice_tooltiptext"><p>Real-time reporting, and dashboards give complete visibility and detailed drill-downs.</p></span>
+            </div>
+            <p className='NLP_h5 '>NLP</p>
+
+            <div className="tooltip">
+                <p className='Visual_h5 '>Visual IVR</p>
+                <span class="Visual_tooltiptext"><p>Real-time reporting, and dashboards give complete visibility and detailed drill-downs.</p></span>
+            </div>
+            <p className='Contextual_h5 '>Contextual conversation</p>
+
+            <div className="tooltip">
+                <p className='Chatbot_h5 '>Chatbot</p>
+                <span class="Chatbot_tooltiptext"><p>Real-time reporting, and dashboards give complete visibility and detailed drill-downs.</p></span>
+            </div>
+            <p className='RPA_h5 '>RPA</p>
+            <p className='Sentiment_h5 '>Sentiment Analyser</p>
+            <h1 className='Entelligent_51 '>Entelligent Automation <br /> Desktop and Service on Cloud </h1>
+
+
+
+
+
+
+
+
+
+            <img src={voice} alt="" className=' voice' />
+
+
+
+
+
+
+
+            <img src={Voice_1} alt="" className=' Voice_1' />
+            <img src={Visual} alt="" className=' Visual' />
+            <img src={Chatbot} alt="" className=' Chatbot' />
+            <img src={line_6} alt="" className=' line_6' />
+            <img src={omni_channel_bg} alt="" className=' omni_channel_bg' />
+            <img src={girl} alt="" className=' girl' />
+            <img src={line_5} alt="" className=' line_5' />
+            <div class="tooltip">
+                <img src={voice_2} alt="" className=' voice_2' />
+                <span class="voice_2_tooltiptext"><p>Real-time reporting, and dashboards give complete visibility and detailed drill-downs.</p></span>
+                <img src={voice_active_2} alt="" className=' voice_active_2' />
+            </div>
+            <div class="tooltip">
+                <img src={sms} alt="" className=' sms' />
+                <span class="sms_tooltiptext"><p>Real-time reporting, and dashboards give complete visibility and detailed drill-downs.</p></span>
+                <img src={sms_active_3} alt="" className=' sms_active_3' />
+            </div>
+            <div class="tooltip">
+                <img src={social_media} alt="" className=' social_media' />
+                <span class="social_media_tooltiptext"><p>Real-time reporting, and dashboards give complete visibility and detailed drill-downs.</p></span>
+                <img src={social_media_active_1} alt="" className=' social_media_active_1' />
+            </div>
+            <div class="tooltip">
+                <img src={white_mail} alt="" className=' white_mail' />
+                <span class="white_mail_tooltiptext"><p>Real-time reporting, and dashboards give complete visibility and detailed drill-downs.</p></span>
+                <img src={white_mail_2} alt="" className=' white_mail_2' />
+            </div>
+            <div class="tooltip">
+                <img src={email} alt="" className=' email' />
+                <span class="email_tooltiptext"><p>Real-time reporting, and dashboards give complete visibility and detailed drill-downs.</p></span>
+                <img src={email_active_2} alt="" className=' email_active_2' />
+            </div>
+            <div class="tooltip">
+                <img src={chat} alt="" className=' chat' />
+                <span class="white_mail_tooltiptext"><p>Real-time reporting, and dashboards give complete visibility and detailed drill-downs.</p></span>
+                <img src={chat_active_2} alt="" className=' chat_active_2' />
+            </div>
+            <h1 className='Omnichannel_h1'>Omnichannel engagement</h1>
+            <div className="channel">
+                <p>select any channel for more details</p>
+            </div>
+            <div className="impowered">
+                <h1>Digitally Impowered Contact Center</h1>
+            </div>
+            <div className="tooltip">
+                <h5 className=' House1_h5 '>associate <br /> at home</h5>
+                <span class="Customer_Experience_tooltiptext"><p>Real-time reporting, and dashboards give complete visibility and detailed drill-downs.</p></span>
+            </div>
+            <div className="tooltip">
+                <p className=' piston_h5 '>Advice bots </p>
+                <span class="piston_tooltiptext"><p>Real-time reporting, and dashboards give complete visibility and detailed drill-downs.</p></span>
+            </div>
+            <div className="tooltip">
+                <p className=' Automation_h5 '>Automation bots </p>
+                <span class="Automation_tooltiptext"><p>Real-time reporting, and dashboards give complete visibility and detailed drill-downs.</p></span>
+            </div>
+
+
+
+
+
+
+
+
+            <div className="tooltip">
+                <p className='Unified_h5 '>Unified View</p>
+                <span class="Unified_tooltiptext"><p>Real-time reporting, and dashboards give complete visibility and detailed drill-downs.</p></span>
+            </div>
+            <div className="tooltip">
+                <h5 className='Associates_h5 '>Associates <br /> in Office</h5>
+                <span class="Associates_tooltiptext"><p>Real-time reporting, and dashboards give complete visibility and detailed drill-downs.</p></span>
+            </div>
+        </div>
+    )
+}
+
+export default Center
